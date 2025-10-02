949f4a3c3e5a3b3cac9edd10dda38461b9c61067
LotsaTella <mochanitrate@gmail.com>
2025-10-02 22:04:33 +0100

    moved atlas funcs into header

--- main.cpp
+++ main.cpp
@@ -2,68 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "headers/atlas.h"
 using namespace std;
-
-typedef struct {
-    char name[64];
-    Rectangle sourceRec;
-} SpriteInfo;
-
-typedef struct {
-    Texture2D texture;
-    SpriteInfo sprites[32];
-    int spriteCount;
-} TextureAtlas;
-
-TextureAtlas LoadAtlas(const char *rtpaPath) {
-    // Initalize atlas
-    TextureAtlas atlas = {0};
-
-    // Open the file specified in the function argument
-    FILE *file = fopen(rtpaPath, "r");
-
-    char line[256];
-    char imagePath[128] = {0};
-
-    // Gets the path of the image and the index of its sprites by reading the lines with "a" and "s" prefixes
-    while (fgets(line, sizeof(line), file)) {
-        if (line[0] == '#') continue;
-
-        if (line[0] == 'a') {
-            sscanf(line, "a %s", imagePath);
-        }
-
-        else if (line[0] == 's') {
-            SpriteInfo *sprite = &atlas.sprites[atlas.spriteCount];
-
-            char nameId[64], tag[64];
-            int originX, originY, posX, posY, srcW, srcH;
-            // We don't need these
-            sscanf(line, "s %s %s %d %d %d %d %d %d", nameId, tag, &originX, &originY, &posX, &posY, &srcW, &srcH);
-
-            strcpy(sprite->name, nameId);
-            sprite->sourceRec = (Rectangle){posX, posY, srcW, srcH};
-
-            atlas.spriteCount++;
-        }
-    }
-
-    fclose(file);
-
-    atlas.texture = LoadTexture(imagePath);
-
-    return atlas;
-}
-
-void DrawAtlasSprite(TextureAtlas atlas, const char *spriteName, Vector2 position) {
-    // Find the sprite within the atlas's sprite array and draw it if found
-    for (int i = 0; i < atlas.spriteCount; i++) {
-        if (strcmp(atlas.sprites[i].name, spriteName) == 0) {
-            DrawTextureRec(atlas.texture, atlas.sprites[i].sourceRec, position, WHITE);
-            break;
-        }
-    }
-}
+using namespace atlas;
 
 int main()
 {
